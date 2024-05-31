0b7dcd829cbf47c53a825361289e5f789b5fff94
Farah Touqan <104039200+farahTouqan@users.noreply.github.com>
2024-06-01 00:07:13 +0300

    Create btb_imports.sv

---
+++ btb_imports.sv
@@ -0,0 +1,14 @@
+package test_pkg;
+  import uvm_pkg::*;
+
+  `include "btb_seq_item.sv"
+  `include "btb_sequencer.sv"
+  `include "btb_driver.sv"
+  `include "btb_monitor.sv"
+  `include "btb_agent.sv"
+  `include "btb_scoreboard.sv"
+  `include "btb_env.sv"
+  `include "btb_test.sv"
+  `include "btb_generic_test.sv"
+
+endpackage
